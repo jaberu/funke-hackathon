8624cc07a76c4c04b419eb81050fede33f1f84e3
Christian Jacobs <christian.jacobs@unitb-consulting.de>
2017-09-08 14:31:15 +0200

    fix SUPPORTED_CATEGORIES

--- app/skillConfig.js
+++ app/skillConfig.js
@@ -1,53 +1,28 @@
 module.exports = {
     APP_ID: '', // TODO replace with your app ID (OPTIONAL)
-    DYNAMODB_TABLE_NAME: 'AmazonNewsAlexaSkill', // TODO customize DynamoDB table name
+    DYNAMODB_TABLE_NAME: 'BerlinerMorgenpostNewsAlexaSkill', // TODO customize DynamoDB table name
     AUDIO_FILE_URL: 'https://s3.amazonaws.com/amazon-headliner/latest_news.mp3', // TODO provide url to your own news audiofile
-    COMPANY_NAME: 'Amazon', // TODO customize company/organization name
-    SKILL_NAME: 'Headliner', // TODO customize skill name
-    BASE_URL: 'https://yomnhwdx8b.execute-api.us-east-1.amazonaws.com/dev/',
+    COMPANY_NAME: 'Funke Digital',
+    SKILL_NAME: 'Berliner Morgenpost News',
+    BASE_URL: 'https://www.morgenpost.de/',
     LATEST_URI: 'latest',
     SUPPORTED_CATEGORIES: {
-        politics: {
-            URI: 'politics',
+        politik: {
+            URI: 'politik',
             synonyms: [
-              'political'
+              'politik'
             ]
         },
-        business: {
-            URI: 'business',
+        berlin: {
+            URI: 'berlin',
             synonyms: [
-              'industry',
-              'trade',
-              'trading',
-              'commerce',
-              'financial',
-              'finance'
+              'berlin'
             ]
         },
-        technology: {
-            URI: 'technology',
+        sport: {
+            URI: 'sport',
             synonyms: [
-              'tech',
-              'technological',
-              'technical'
-            ]
-        },
-        science: {
-            URI: 'science',
-            synonyms: [
-              'scientific',
-              'stem',
-              'engineering',
-              'environment',
-              'environmental',
-              'climate',
-              'nature'
-            ]
-        },
-        design: {
-            URI: 'design',
-            synonyms: [
-              'designer'
+              'sport'
             ]
         }
     }
